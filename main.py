109d9a95e3bcc4e4771c2c8b89c63ef4ed691bb9
jj <someone236632@gmail.com>
2025-03-07 14:56:28 +0300

    g

---
+++ main.py
@@ -0,0 +1,51 @@
+from g4f.client import Client
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+client = Client()
+
+
+def get(text):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": text}],
+        web_search=False,
+    )
+    return response.choices[0].message.content
+
+
+def get_image(name):
+    response = client.images.generate(
+        model="flux",
+        prompt=f"Картинка для поздравления на 8 марта по теме: {name}",
+        response_format="url",
+        temperature=0.5
+    )
+
+    return response.data[0].url
+
+
+@app.route('/')
+def hello_world():
+    resp = get(
+        "Напиши стихотворение-поздравление на 8 марта для Елены от 12 до 30 строчек, где первая строчка - это название стихотворения. Название не должно содержать имен и не должно сожержать сиивол '*'").split(
+        "\n")
+    print(resp)
+    if "openai" in resp[0].lower():
+        resp = resp[1:]
+        while not resp[0]:
+            resp = resp[1:]
+    text = resp[1:]
+    while not resp[0]:
+        resp = resp[1:]
+    title = resp[0]
+    image = get_image(title)
+    return render_template("index.html",
+                           title=title,
+                           text=text,
+                           image=image)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
